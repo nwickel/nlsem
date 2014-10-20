c4ddcb8ab36f98acbab158abc1f5b3e381ffe05a
Kathi <kathi.naumann@t-online.de>
2014-10-20 12:09:20 +0200

    added mu_stemm function

---
+++ R/stemm.R
@@ -0,0 +1,25 @@
+# stemm.R
+#
+# created: Okt/20/2014, KN
+# last mod: Okt/20/2014, KN
+
+mu_stemm <- function(model, group) {
+    stopifnot(class(model) == "stemmFilled")
+
+    matrices <- model$matrices[[group]]
+
+    # TODO case for no y's (as in Davids code). Necessary here?
+    # TODO catch error if B is not nonsingular
+    mu.y <- matrices$nu.y + matrices$Lambda.y %*% solve(matrices$Beta) %*%
+            (matrices$alpha + matrices$Gamma %*% matrices$tau)
+    mu.x <- matrices$nu.x + matrices$Lambda.x %*% matrices$tau
+    mu <- c(mu.y, mu.x)
+    # mu <- rbind(mu.y, mu.x) # vertical vector
+
+    mu
+}
+
+sigma_stemm <- function(model, group) {
+
+}
+
