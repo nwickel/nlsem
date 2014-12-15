7aa5ef69e5966efd4480a314b30e5f31fcf233cb
Kathi <kathi.naumann@t-online.de>
2014-12-15 15:58:17 +0100

    small changes in semm tests

--- tests/tests_for_semm.R
+++ tests/tests_for_semm.R
@@ -3,40 +3,26 @@
 
 library(nlsem)
 
+# SEMM model for structural equation models
+# ==========================================
+
 # create model
-# ------------
-model <- specify_sem(num.x=4, num.y=4, num.xi=2, num.eta=2,
-                     xi="x1-x2,x3-x4", eta="y1-y2,y3-y4", num.classes=3,
-                     interaction="", interc.obs=FALSE, interc.lat=FALSE,
-                     relation.lat="eta1~xi1,eta2~xi2")
-set.seed(188)
-pars.orig <- runif(count_free_parameters(model), 0.3, 1.2)
-data <- simulate(model, parameters=pars.orig)
-
-set.seed(188)
-pars.start <- runif(count_free_parameters(model), 0.1, 1.5)
-# system.time(
-#     res.nlminb <- em(model, data, pars.start, logger=TRUE, optimizer="nlminb")
-# )
-# system.time(
-#     res.optim <- em(model, data, pars.start, logger=TRUE, optimizer="optim")
-# )
-
-# EMSEM example: STEMM model for structural equation models
-# =========================================================
 mod <- specify_sem(num.x=4, num.y=4, num.xi=2, num.eta=2,
                      xi="x1-x2,x3-x4", eta="y1-y2,y3-y4", num.classes=2,
                      interaction="",
-                     interc.obs=TRUE, interc.lat=FALSE,
+                     interc.obs=FALSE, interc.lat=FALSE,
                      relation.lat="eta1~xi1,eta2~xi2,eta2~eta1,eta1~eta2")
 
 dat <- as.data.frame(mod)
 
-dat[c(2,8,10,16),2:3] <- 1    # Lambda.x.21 & 42 and Lambda.y.21 & 42
-dat[c(58,62),2:3]     <- 0    # psi.21 and phi.21
-dat[75:76,2:3]        <- NA   # tau
+dat[c(2,8,10,16),2:3]   <- 1    # Lambda.x.21 & 42 and Lambda.y.21 & 42
+dat[c(58,62),2:3]       <- 0    # psi.21 and phi.21
+dat[75:76,2:3]          <- NA   # tau
+dat[65:74,2:3]          <- 1    # nu.x and nu.y
 
 model <- create_sem(dat)
+
+# simulate data
 parameters <- c(
                 # class 1
                 rep(0.5, 2),    # Gamma
@@ -50,16 +36,22 @@ parameters <- c(
                 rep(1, 2),      # Phi
                 rep(4, 2)       # tau
 )
-set.seed(1)
+data <- simulate(model, seed=7, parameters=parameters)
+
+# estimate model
+
+set.seed(8)
 parameters <- runif(count_free_parameters(model), 0.1, 1.5)
-set.seed(7)
-data <- simulate(model, parameters=parameters)
+
+# system.time(
+#     res <- em(model, data, parameters, logger=TRUE, max.iter=1000)
+# )
 
 # system.time({
 #     res <- em(model, data, parameters, logger=TRUE, max.iter=1000)
 # })
 
-# small model
+# smaller model
 # ------------
 model <- specify_sem(num.x=4, num.y=2, num.xi=2, num.eta=1,
                      xi="x1-x2,x3-x4", eta="y1-y2", num.classes=2,
@@ -85,12 +77,12 @@ pars.orig <- c(
                      rep(0.8, 3)    # Phi
 )
 
-data <- simulate(model, parameters=pars.orig)
+data <- simulate(model, seed=1, parameters=pars.orig)
 
 set.seed(3)
 parameters <- runif(count_free_parameters(model), 0.1, 1.5)
 
 # system.time({
-#     res<- em(model, data, parameters, logger=TRUE, max.mstep=10)
+#     res<- em(model, data, parameters, logger=TRUE, max.mstep=1)
 # })
 
