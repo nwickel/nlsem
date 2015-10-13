ba5eab0cd2455c933ce4016854415ed9385cf98d
Nora Umbach <nora.umbach@web.de>
2015-10-13 10:18:59 +0200

    [BUGFIX] Fixed output of qml

--- R/qml.R
+++ R/qml.R
@@ -36,7 +36,7 @@ qml <- function(model, data, start, max.iter=150,
                 neg.hessian=est$hessian,
                 iterations=est$iterations,
                 info=model$info[c("num.xi", "num.eta", "num.x", "num.y",
-                "xi", "eta", "num.classes")])
+                "constraints", "num.classes")])
 
     class(out) <- "qmlEst"
     out
